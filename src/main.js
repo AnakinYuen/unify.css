0e0afdb08b11ec76c36c4af61d76db0c8c9edf6d
Anakin Yuen <anakinyuen1024@gmail.com>
2020-01-26 03:18:36 +0800

    :construction: wip: add writable store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
+import { json } from './store.js';
 import App from './App.svelte';
+
+const getUnifyJson = async () => {
+  const response = await fetch('./unify.json');
+  const rawJson = await response.json();
+  Object.keys(rawJson).forEach(key => {
+    rawJson[key].forEach(ruleSets => {
+      ruleSets.declarations.forEach(declaration => {
+        declaration.disable = false;
+        if (declaration.options) {
+          declaration.options.unshift({
+            value: declaration.value,
+            comment: declaration.comment,
+            reference: declaration.reference,
+          });
+        }
+      });
+    });
+  });
+  return rawJson;
+};
+
+getUnifyJson().then(rawJson => json.set(rawJson));
 
 const app = new App({
   target: document.body,
-  props: {
-    name: 'world',
-  },
 });
 
 export default app;
